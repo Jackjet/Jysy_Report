ea6b27cde70c0f5aa1aa450d020e6e7b29fa8402
coderss <admin@coderss.cn>
2017-07-04 11:26:12 +0800

    up

--- src/main/java/cn/coderss/jysy/controller/FbhyController.java
+++ src/main/java/cn/coderss/jysy/controller/FbhyController.java
@@ -2,11 +2,16 @@ package cn.coderss.jysy.controller;
 
 import cn.coderss.jysy.service.FbhyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Optional;
 
 /**
  * Created with report.
@@ -27,6 +32,14 @@ public class FbhyController {
         return "redirect:/"+fileName;
     }
 
+    @GetMapping(value = "/test")
+    public ResponseEntity<HashMap<String,String>> getUserAccounts() throws Exception {
+        return Optional.ofNullable(new HashMap<String, String>() {{
+            put("message", "code");
+            put("states", "1");
+        }}).map(data -> new ResponseEntity<HashMap<String, String>>(data, HttpStatus.OK))
+                .orElseThrow(() -> new Exception("data error"));
+    }
 //    @GetMapping(value = "/loginreport_stream")
 //    public InputStream getLoginDataStream(){
 //        fbhyService.loginLogReportStream();
