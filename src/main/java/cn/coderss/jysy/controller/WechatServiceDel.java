41ce33114d0322c35d5701d6eb265117531e65ac
coderss <admin@coderss.cn>
2017-07-06 15:05:03 +0800

    up

--- src/main/java/cn/coderss/jysy/controller/WechatServiceDel.java
+++ src/main/java/cn/coderss/jysy/controller/WechatServiceDel.java
@@ -1,10 +1,14 @@
 package cn.coderss.jysy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,8 +23,12 @@ import java.util.regex.Pattern;
 @RequestMapping("/wechat")
 public class WechatServiceDel {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @CrossOrigin(value = "*")
     @RequestMapping(value = "/del", method = RequestMethod.POST)
-    public static HashMap<String,String> delService(String content){
+    public String delService(String content){
         String regex = "([a-z]|[A-Z]|[0-9])*";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(content);
@@ -31,29 +39,42 @@ public class WechatServiceDel {
             }
         }
         stringBuilder.delete(stringBuilder.length()-1,stringBuilder.length());
-        return new HashMap<String,String>(){{
-            put("message", stringBuilder.toString());
-        }};
+
+        stringBuilder.delete(stringBuilder.length()-1,stringBuilder.length());
+        String sql = "SELECT `info`.`infoid`\n" +
+                "FROM `vmb_account` as `account`\n" +
+                "INNER JOIN `wechat_service`.`wechat_user_info` as `info` on `info`.`accountId`  = `account`.`accountId` \n" +
+                "WHERE `account`.`name` in (\n" +
+                stringBuilder.toString()+");\n";
+
+        List<String> data =this.jdbcTemplate.query(sql, (rs, num)->
+            new String("delete from `wechat_service`.`wechat_user_info` " +
+                    "where `infoid` ="+rs.getString("infoid"))
+        );
+
+        for (String itemSql : data){
+            this.jdbcTemplate.execute(itemSql);
+        }
+
+
+        sql =  "SELECT `token`.*\n" +
+                "FROM `vmb_account` as `account`\n" +
+                "INNER JOIN `wechat_service`.`wechat_service_token`  as `token` on `token`.`accountId`  = `account`.`accountId` \n" +
+                "WHERE `account`.`name` in (\n" +
+                stringBuilder+");";
+        data = this.jdbcTemplate.query(sql ,(rs, num)->
+                new String("delete from `wechat_service`.`wechat_service_token` " +
+                "where `tokenid` ="+rs.getString("tokenid"))
+        );
+
+        for (String itemSql : data){
+            this.jdbcTemplate.execute(itemSql);
+        }
+
+        return "ok";
     }
 
     public static void main(String[] args) {
-        System.out.println(WechatServiceDel.delService("FUZN2813\t颜金土\n" +
-                "FUZGR536\t林金珠\n" +
-                "FUZSD017\t黄丽萍\n" +
-                "FUZT1468\t庄碧华\n" +
-                "FUZT4387\t陈秀梅\n" +
-                "FUZT5616\t许爱平\n" +
-                "FUZTE710\t赖雪君\n" +
-                "FUZU0389\t施瑞珊\n" +
-                "FUZY0150\t王文智\n" +
-                "FUZK1838\t赖玉华\n" +
-                "FUZK5099\t李莹香\n" +
-                "FUZK2773\t何春芳\n" +
-                "FUJH2210\t李菊女\n" +
-                "FUZP0287\t季思恩\n" +
-                "FUZG2525\t庄素琼\n" +
-                "FUZTE218\t肖美英\n" +
-                "FUZZ3630\t林志英\n" +
-                "FUZZ8441\t王妥霞"));
+
     }
 }
